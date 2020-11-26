6e91bce89810b17fe8f87d404b6883206834e6d5
Louis D'Hont <louis.dhont@student.odisee.be>
2020-11-26 15:22:45 +0100

    Testing autobuild

--- Blok1/ConsoleApp/ConsoleApp/Program.cs
+++ Blok1/ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from C#");
+            Console.WriteLine("Hello World from C#!");
         }
     }
 }
