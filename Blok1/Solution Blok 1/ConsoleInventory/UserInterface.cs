69310f5771f6d57f9ec2652fcff573533e19f741
Louis D'Hont <louis.dhont@student.odisee.be>
2020-11-28 16:24:11 +0100

    Removed class libraries

--- Blok1/Solution Blok 1/ConsoleInventory/UserInterface.cs
+++ Blok1/Solution Blok 1/ConsoleInventory/UserInterface.cs
@@ -16,8 +16,19 @@ namespace ConsoleInventory
 
         private void ShowBanner()
         {
-            Console.WriteLine("Console Inventory");
-            Console.WriteLine("--------------\n");
+            Console.WriteLine("Console Inventory Application");
+            Console.WriteLine("-----------------------------\n");
+
+            Double AmountEuro, AmountUsd;
+            const Double Wisselkoers = 1.2231;
+
+            Console.WriteLine("Enter your amount that you want to convert in Euro: ");
+            AmountEuro = Convert.ToDouble(Console.ReadLine());
+
+            AmountUsd = AmountEuro * Wisselkoers;
+
+            Console.WriteLine($"Your €{AmountEuro.ToString()} is equal to ${AmountUsd.ToString()}");
+            Console.ReadLine();
         }
     }
 }
--- Blok1/Solution Blok 1/Inventory/MainForm.cs
+++ Blok1/Solution Blok 1/Inventory/MainForm.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-namespace Inventory
+namespace InventoryMain
 {
     public partial class MainForm : Form
     {
 
-        
         public MainForm()
         {
-            InitializeComponent();
-
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
